7939a7ab70fe12aac523588c6aef3d7c00aa3120
peisns <hansol1501@gmail.com>
2022-08-03 18:41:22 +0900

    check ImageSearchViewController

---
+++ SeSAC2LectureNetworkBasic/ImageSearchCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageSearchCollectionViewCell.swift
+//  SeSAC2LectureNetworkBasic
+//
+//  Created by Brother Model on 2022/08/03.
+//
+
+import UIKit
+
+class ImageSearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var searchedImageView: UIImageView!
+    
+}
--- SeSAC2LectureNetworkBasic/ImageSearchViewController.swift
+++ SeSAC2LectureNetworkBasic/ImageSearchViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 import Alamofire
+import Kingfisher
 import SwiftyJSON
 
 class ImageSearchViewController: UIViewController {
 
+    @IBOutlet weak var ImageCollectionView: UICollectionView!
+    
+    var imageURL: String = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ImageCollectionView.delegate = self
+        ImageCollectionView.dataSource = self
+        
+        // design cell layout
+        let cellLayout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 16
+        let screenWidth = UIScreen.main.bounds.width - (spacing * 2)
+        cellLayout.itemSize = CGSize(width: screenWidth , height: screenWidth)
+        cellLayout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        cellLayout.minimumLineSpacing = spacing
+        cellLayout.minimumInteritemSpacing = spacing
+
+        ImageCollectionView.collectionViewLayout = cellLayout
 
         fetchImage()
     }
@@ -31,10 +50,10 @@ class ImageSearchViewController: UIViewController {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
+                let imageURL = json["items"][0]["link"].stringValue
+                self.imageURL = imageURL
                 
-                
-                
-                
+                self.ImageCollectionView.reloadData()
             case .failure(let error):
                 print(error)
             }
@@ -42,4 +61,24 @@ class ImageSearchViewController: UIViewController {
     }
     
 
+}
+
+
+extension ImageSearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageSearchCollectionViewCell.reuseIdentifier, for: indexPath) as? ImageSearchCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.backgroundColor = .orange
+        let searchedImageURL = URL(string: imageURL)
+        cell.searchedImageView.kf.setImage(with: searchedImageURL)
+        return cell
+    }
+}
+
+extension ImageSearchViewController: UISearchBarDelegate {
+    
 }
